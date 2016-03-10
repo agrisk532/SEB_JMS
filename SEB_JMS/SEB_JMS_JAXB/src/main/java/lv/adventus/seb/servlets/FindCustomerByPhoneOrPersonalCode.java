153088f9320fab9111c8053469dd9a56caf9531e
Agris Klimkans <agris.klimkans@adventus.lv>
2016-03-10 13:51:57 +0200

    FindCustomerByPhoneOrPersonalCode() service done. Compiles. To be done: Refactoring

--- SEB_JMS/SEB_JMS_JAXB/src/main/java/lv/adventus/seb/servlets/FindCustomerByPhoneOrPersonalCode.java
+++ SEB_JMS/SEB_JMS_JAXB/src/main/java/lv/adventus/seb/servlets/FindCustomerByPhoneOrPersonalCode.java
@@ -27,6 +27,7 @@ import lv.adventus.seb.util.MultipartMessageUtility;
 import lv.adventus.seb.UnifiedServiceResponse;
 import lv.adventus.seb.UnifiedServiceErrors;
 import lv.adventus.seb.ContactcenterFindCustomerByPhoneOrPersonalCode2Output;
+import lv.adventus.seb.ContactcenterGiveDigipassChallenge2Output;
 
 /**
  * Servlet implementation class FindCustomerByPhoneOrPersonalCode
@@ -34,13 +35,17 @@ import lv.adventus.seb.ContactcenterFindCustomerByPhoneOrPersonalCode2Output;
 public class FindCustomerByPhoneOrPersonalCode extends HttpServlet {
 	
     private static final String broker = "awebs:6526";
-    private static final String username = "tester1";
-    private static final String password = "tester";
+    private static final String usernameSonic = "tester1";
+    private static final String passwordSonic = "tester";
     private static final String queue = "SEB_SERVICES";
     private static final long timeout = 60000;
 	private String xmlrequest;
-	private String xmlresponseFindCustomer;
-	
+	private String xmlresponse;
+	private String customerId;
+	private String idCode;
+	private String challengeCode;
+	private String userName;
+
 
     /**
      * Default constructor. 
@@ -119,7 +124,7 @@ public class FindCustomerByPhoneOrPersonalCode extends HttpServlet {
 	    	return;
 		}
 		
-		Connector c = new Connector(broker,username,password,queue, out, timeout);
+		Connector c = new Connector(broker,usernameSonic,passwordSonic,queue, out, timeout);
 		c.start();
 	    //start(broker,username,password,queue, out);
 	    try
@@ -135,19 +140,19 @@ public class FindCustomerByPhoneOrPersonalCode extends HttpServlet {
 			}
 			MultipartMessageUtility mmu = new MultipartMessageUtility(c); 
 			mmu.onMessage(responseMsg);
-			xmlresponseFindCustomer = mmu.getXMLMessage();
+			xmlresponse = mmu.getXMLMessage();
 			
-//////////  analyze xmlresponseFindCustomer ///////////////
+//////////  analyze xmlresponse ///////////////
 			
 			JAXBContext jaxbContext = JAXBContext.newInstance(UnifiedServiceResponse.class);
 		    Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
 		 // mangle namespaces in the xmlDocument
-		      int pos = xmlresponseFindCustomer.indexOf("UnifiedServiceResponse") + (new String("UnifiedServiceResponse")).length();
-		      int pos2 = xmlresponseFindCustomer.indexOf(">", pos);
-		      xmlresponseFindCustomer =
-		    		  xmlresponseFindCustomer.replace(xmlresponseFindCustomer.substring(pos, pos2), " xmlns=\"http://www.seb.ee/integration\"");
+		      int pos = xmlresponse.indexOf("UnifiedServiceResponse") + (new String("UnifiedServiceResponse")).length();
+		      int pos2 = xmlresponse.indexOf(">", pos);
+		      xmlresponse =
+		    		  xmlresponse.replace(xmlresponse.substring(pos, pos2), " xmlns=\"http://www.seb.ee/integration\"");
 		      // unmarshal
-		      InputStream stream = new ByteArrayInputStream(xmlresponseFindCustomer.getBytes());
+		      InputStream stream = new ByteArrayInputStream(xmlresponse.getBytes());
 		      UnifiedServiceResponse usr = (UnifiedServiceResponse) unMarshaller.unmarshal(stream); 
 	    	  UnifiedServiceErrors errors = usr.getUnifiedServiceErrors();
 	    	  if(errors != null)
@@ -165,15 +170,97 @@ public class FindCustomerByPhoneOrPersonalCode extends HttpServlet {
 	    	  }
 		      ContactcenterFindCustomerByPhoneOrPersonalCode2Output o =
 	    			  (ContactcenterFindCustomerByPhoneOrPersonalCode2Output) usr.getUnifiedServiceBody().getAny().get(0);
-	    	  String customerId = o.getFindCustomerResponse().getCustomerId();
-	    	  String idCode = o.getFindCustomerResponse().getIdCode();
-	    	  System.out.println(customerId);
-	    	  System.out.println(idCode);
+	    	  this.customerId = o.getFindCustomerResponse().getCustomerId();
+	    	  this.idCode = o.getFindCustomerResponse().getIdCode();
+	    	  System.out.println(this.customerId);
+	    	  System.out.println(this.idCode);
   			  c.exit();
+        }
+	    catch (JAXBException e)
+	    {
+	    	System.err.println(e.getMessage());
+	    }
+        catch (javax.jms.JMSException jmse)
+        {
+        	out.println("TECHNICALERROR");
+        	System.out.println(jmse);
+        	return;
+        }
 
-	    	  // create request to the GiveDigipassChallenge service
-			
-
+	      // create request to the GiveDigipassChallenge service
+		
+  		try
+  		{
+  			lv.adventus.seb.GiveDigipassChallenge dc = new lv.adventus.seb.GiveDigipassChallenge();
+  			dc.SetHeader();
+  			dc.SetHeaderUserId(userId);
+  			dc.SetHeaderRequestId(connId);
+  			dc.SetBody();
+  			dc.SetBody(this.customerId, this.idCode);
+  			xmlrequest = dc.Marshal();
+  		}
+  		catch (JAXBException e)
+  		{
+  			System.out.println(e.getMessage());
+  			out.println("TECHNICALERROR");
+  			return;
+  		}
+  		
+  		c = new Connector(broker,usernameSonic,passwordSonic,queue, out, timeout);
+  		c.start();
+  		//start(broker,username,password,queue, out);
+  		try
+  	    {
+  	    	progress.message.jclient.XMLMessage msg = ((progress.message.jclient.Session) c.getSession()).createXMLMessage();
+  			//javax.jms.TextMessage msg = session.createTextMessage();
+  			msg.setText( xmlrequest );
+  			// Instead of sending, we will use the QueueRequestor.
+  			javax.jms.Message responseMsg = c.getRequestor().request(msg, timeout);
+  			if(responseMsg == null)
+  			{
+  				throw new javax.jms.JMSException("No response from JMS broker");
+  			}
+  			MultipartMessageUtility mmu = new MultipartMessageUtility(c); 
+  			mmu.onMessage(responseMsg);
+  			xmlresponse = mmu.getXMLMessage();
+  			
+  	//////  analyze xmlresponse ///////////////
+  			
+  			JAXBContext jaxbContext = JAXBContext.newInstance(UnifiedServiceResponse.class);
+  		    Unmarshaller unMarshaller = jaxbContext.createUnmarshaller();
+  		 // mangle namespaces in the xmlDocument
+  		      int pos = xmlresponse.indexOf("UnifiedServiceResponse") + (new String("UnifiedServiceResponse")).length();
+  		      int pos2 = xmlresponse.indexOf(">", pos);
+  		      xmlresponse =
+  		    		  xmlresponse.replace(xmlresponse.substring(pos, pos2), " xmlns=\"http://www.seb.ee/integration\"");
+  		      // unmarshal
+  		      InputStream stream = new ByteArrayInputStream(xmlresponse.getBytes());
+  		      UnifiedServiceResponse usr = (UnifiedServiceResponse) unMarshaller.unmarshal(stream); 
+  			  UnifiedServiceErrors errors = usr.getUnifiedServiceErrors();
+  			  if(errors != null)
+  			  {
+  				  String err = "There are errors";
+  				  for (int i = 0; i < errors.getError().size(); i++)
+  				  {
+  					  err = errors.getError().get(i).getErrorClass();
+  					  System.out.println(err);
+  					  System.out.println(errors.getError().get(i).getErrorCode());
+  					  System.out.println(errors.getError().get(i).getErrorObject().getValue());
+  				  }
+  		    	  out.println(err);
+  		    	  return;
+  			  }
+  			  ContactcenterGiveDigipassChallenge2Output o = (ContactcenterGiveDigipassChallenge2Output) usr.getUnifiedServiceBody().getAny().get(0);
+  			  this.customerId = o.getGiveChallengeResponse().getCustomerId();
+  			  System.out.println(this.customerId);
+  			  this.challengeCode = o.getGiveChallengeResponse().getChallengeCode(); 
+  			  System.out.println(this.challengeCode);
+  			  this.userName = o.getGiveChallengeResponse().getUsername();
+  			  System.out.println(this.userName);
+  			  this.idCode = o.getGiveChallengeResponse().getIdCode();
+  			  System.out.println(this.idCode);
+  			  c.exit();
+  			  out.println("<"+this.challengeCode+">");
         }
         catch (javax.jms.JMSException jmse)
         {
